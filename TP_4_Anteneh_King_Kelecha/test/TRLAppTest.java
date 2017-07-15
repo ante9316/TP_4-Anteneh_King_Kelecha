38d20b744e79265c7252e308b59e7b986ca164e6
asheant <asheant@USRTC-STEVTOW6.emrsn.org>
2017-07-15 14:23:07 -0500

    Removing UI test case

--- TP_4_Anteneh_King_Kelecha/test/TRLAppTest.java
+++ TP_4_Anteneh_King_Kelecha/test/TRLAppTest.java
@@ -4,26 +4,6 @@ public class TRLAppTest
 {
 
 	@Test
-	public void testValidatePatron()
-	{
-		StdOut.println("Valid Patron can be used:-  P1, P2, P3");
-		StdOut.println("In Valid Patron can be used:-  P10");
-		CheckInOutController controller = new CheckInOutController();
-
-		TRLApp.validatePatron(controller);
-
-	}
-
-	@Test
-	public void testValidCopy()
-	{
-		StdOut.println("In Valid Patron can be used:-  C10");
-		StdOut.println("Valid Copy can be used:-  C1, C2,C3");
-		CheckInOutController controller = new CheckInOutController();
-		TRLApp.validateCopies(controller);
-	}
-
-	@Test
 	public void testPrintHold()
 	{
 		Copy copy1 = new Copy("C1", "Testing");
